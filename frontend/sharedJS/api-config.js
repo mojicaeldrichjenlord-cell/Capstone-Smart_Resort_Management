9771caff501e4b52c6ea93b74d1e40c5a1f51870
Eldrich Mojica <mojicaeldrichjenlord@gmail.com>
2026-06-01 15:55:14 +0800

    render api again

--- frontend/sharedJS/api-config.js
+++ frontend/sharedJS/api-config.js
@@ -1,3 +1,6 @@
 // const API_BASE = "http://127.0.0.1:5000/api"; 
+// taskkill /F /IM node.exe
+//cd backend
+//node server.js
 // const API_BASE = "https://smartresort-backend.onrender.com/api";
 const API_BASE = "https://smartresort-backend.onrender.com/api";
